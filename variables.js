555f1c271d73c4c0d33db4b2b78db201fafb01d5
MichelecFernandes <michelecristinafr@gmail.com>
2026-03-19 22:10:05 -0300

    docs: explain type conversion

--- variables.js
+++ variables.js
@@ -29,4 +29,24 @@ let pessoa = {
 };
 console.log("Pessoa: ", pessoa);    
 
+// Conversao de tipos
+let numeroString = "100";
+let numero = Number(numeroString);
+console.log("Número convertido: " + numero);
+
+
+// Isso acontece porque:
+// + com string costuma juntar textos
+// -, * e / só fazem sentido matemático, então o JS tenta converter a string para número, e se conseguir, ele faz a operação normalmente. Se não conseguir, ele retorna NaN (Not a Number).
+console.log("2" + 2); // "22"
+console.log("2" - 2); // 0
+console.log("2" * 2); // 4
+console.log("2" / 2); // 1
+
+// Conversão de forma explicita usando parseFloat e parseInt:
+console.log(parseFloat("3.14")); // 3.14
+console.log(parseInt("42")); // 42
+console.log(parseInt("42.5")); // 42
+console.log(parseInt("abc")); // NaN
+
 
