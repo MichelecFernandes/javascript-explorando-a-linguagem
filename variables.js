3235cc02f80d8ca294d810c774ba5ca7bcbf48e6
MichelecFernandes <michelecristinafr@gmail.com>
2026-03-19 21:54:27 -0300

    docs: file about variables in JS

---
+++ variables.js
@@ -0,0 +1,32 @@
+console.log("Olá, Mundo! Este é meu primeiro programa em JavaScript.");
+
+// No JS é boa pratica utilizar camelCase para nomear variáveis.
+
+// Se eu não colocar o tipo da variável, o JS irá inferir o tipo automaticamente, mas é recomendado usar let ou const para declarar variáveis. Se não informatar o tipo, o JS irá considerar a variável como var, do tipo global.
+console.log("Vamos aprender sobre variáveis em JavaScript!");
+
+// Variável do tipo string
+let nome = "João";
+console.log("O nome é: " + nome);
+
+// Variável do tipo number
+let idade = 30;
+console.log("A idade é: " + idade);
+
+// Variável do tipo boolean
+let isStudent = true;
+console.log("É estudante? " + isStudent);
+
+// Variável do tipo array
+let frutas = ["maçã", "banana", "laranja"];
+console.log("Frutas: " + frutas);
+
+// Variável do tipo objeto
+let pessoa = {
+    nome: "Maria",
+    idade: 25,
+    profissao: "Engenheira"
+};
+console.log("Pessoa: ", pessoa);    
+
+
